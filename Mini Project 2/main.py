b35096ceb8450cf18abef98980b850098dd80322
giroux2000 <giroux2000@gmail.com>
2022-11-16 23:02:22 -0500

    Reading of file and creating helper functions

---
+++ Mini Project 2/main.py
@@ -0,0 +1,73 @@
+
+import numpy as np
+
+# Open the input file to get values
+with open('inputfile.txt') as f:
+    lines = f.readlines()
+
+# Initialization of variables to track data
+array = np.empty((6,6), dtype=object)  # Array that holds the car positions
+arrcount = 0  # Used to place the car value at the correct index
+gascount = 0  # Used to count which gas value we are currently reading
+carsingame = []  # Used to keep track of all the car names that are in the game
+cargas = {}  # Dictionary containing all the gas values for cars with restricted car values.
+horver = {}  # Dictionary which tracks if the car can move horizontally or vertically.
+carsizes = {} # Dictionary containing the size of all cards.
+carrows = {} # Idk if we would want these  but might be easy to make a list for each row and column detailing which cars can move in
+            # that direction so that we just search for . and then we know how to move it.
+carcols = {}
+
+# Read through every line, we split each line into words based on empty space.
+for line in lines:
+    word = line.split(' ')
+    # Check to make sure that the line isn't a comment or empty.
+    if "#" not in word[0]:
+        if line.strip():
+            # Move every character in the word into a 6 x 6 array.
+            for char in word[0]:
+                row = int(arrcount/6) % 6
+                col = arrcount % 6
+                # Add car name values that have not been added yet to carsingame.
+                if char not in carsingame and char != '.' and char != '\n':
+                    carsingame.append(char)
+                arrcount = arrcount+1
+                array[row, col] = char
+
+        gascount = 0
+        # Take all the words after the first as that is where the gas values begin.
+        word = word[1:]
+        # For each word found after, we append the value to a cargas dictionary.
+        for words in word:
+            print("word: ")
+            print(words)
+            currcarletter = word[gascount][0]
+            currcargas = int(word[gascount][1])
+            cargas[currcarletter] = currcargas
+            gascount = gascount+1
+
+# Go through all the cars in the game and create a dictionary which tracks which way it can move.
+for car in carsingame:
+    currarr = np.argwhere(array == car)
+    row1 = currarr[0][0]
+    row2 = currarr[1][0]
+    if row1 == row2:
+        horver[car] = "h"
+    else:
+        horver[car] = "v"
+
+# Create dictionary with size of all cars.
+for car in carsingame:
+    currarr = np.argwhere(array == car)
+    carsizes[car] = int(np.prod(currarr.shape)/2)
+
+
+# Checks if the game is done based on if the ambulance is in its final position.
+def isGameDone( currArray ):
+    if currArray[2][5] == 'A' and currArray[2][4] == 'A':
+        return True
+    else:
+        return False
+
+def moveCar(car, direction, distance, currentarray): # How do we want to take gas into account
+
+
