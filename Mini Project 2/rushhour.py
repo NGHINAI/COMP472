fe49ea78dda7a602a8829ee32c193ac441c45ad8
giroux2000 <giroux2000@gmail.com>
2022-12-02 17:18:35 -0500

    My comments

--- Mini Project 2/rushhour.py
+++ Mini Project 2/rushhour.py
@@ -5,6 +5,7 @@ import time
 class puzzle:
     openList_gamestate = []
 
+    #Defining puzzle objects
     def __init__(self, array, gas):
         self.array = array
         self.gas = gas
@@ -21,7 +22,6 @@ class puzzle:
     # Checks if the game is done based on if the ambulance is in its final position.
     def isgamedone(self, currarray):
         if currarray[2][5] == 'A' and currarray[2][4] == 'A':
-
             return True
         else:
             return False
@@ -84,6 +84,8 @@ class puzzle:
             return False
 
     def removeValet(self, valremove):
+        #This function removes cars from the valet spotby seeing the value at the valet spot and then removing all the
+        #instances from the game.
         removeVal = valremove[2][5]
         ambulancerow = valremove[2]
         for x in range(0, 6):
@@ -91,39 +93,54 @@ class puzzle:
                 ambulancerow[x] = '.'
 
     def possmoves(self, statearray, gasarray):
+        #This function returns all the possible moves (of length one) possible at a state. We first look at all the
+        #empty spots in the game.
         emptyspots = np.argwhere(statearray == ".")
         possmoves = []
+        #Iterate through every empty spot.
         for spot in emptyspots:
+            #Checking fo horizontal moves.
             horizontalrow = statearray[spot[0]]
+            #Check the spot to the right as long as we aren't at the right most column.
             if (spot[1] < 5):
                 spottoright = horizontalrow[spot[1] + 1]
+                #Check the value to the right, if it's a car that moves horizontally and has gas, it can move to
+                #the left 1 spot.
                 if (spottoright != "." and self.horver[spottoright] == "h"):
                     if (gasarray[spottoright] >= 1):
                         possmoves.append([spottoright, "L", 1])
+            #Check the spot to the left as long as we aren't at the left most column.
             if (spot[1] > 0):
                 spottoleft = horizontalrow[spot[1] - 1]
+                #Check the value to the left, if it's a car that moves horizontally and has gas, it can move to
+                #the right 1 spot.
                 if (spottoleft != '.' and self.horver[spottoleft] == "h"):
                     if (gasarray[spottoleft] >= 1):
                         possmoves.append([spottoleft, "R", 1])
 
+            #Checking vertical moves.
             verticalcol = statearray[:, spot[1]]
+            #Check that we aren't at the top most row.
             if (spot[0] < 5):
                 spotbelow = verticalcol[spot[0] + 1]
+                #If there is a vertical car below with gas, it can move up once.
                 if (spotbelow != "." and self.horver[spotbelow] == "v"):
                     if (gasarray[spotbelow] >= 1):
                         possmoves.append(([spotbelow, "U", 1]))
+
+            #Check that we aren't at the bottom most row
             if (spot[0] > 0):
                 spotabove = verticalcol[spot[0] - 1]
+                #If there is a vertical car above with gas, it can move down once.
                 if (spotabove != "." and self.horver[spotabove] == "v"):
                     if (gasarray[spotabove] >= 1):
                         possmoves.append(([spotabove, "D", 1]))
-
-        # print(possmoves)
         return possmoves
 
     def movecar(self, gamestate, movedetails):
         temparr = copy.deepcopy(gamestate)
-        # print(f"game state: \n{temparr} \n with {movedetails}")
+
+        #Moves the car based on the move details, will shift all the values of the car by one in that direction.
         if (movedetails[1] == "L"):
             carplacement = np.argwhere(gamestate == movedetails[0])
             leftmostcar = carplacement[0]
@@ -149,7 +166,6 @@ class puzzle:
             bottommostcar = carplacement[len(carplacement) - 1]
             temparr[upmostcar[0], upmostcar[1]] = '.'
             temparr[bottommostcar[0] + 1, bottommostcar[1]] = movedetails[0]
-        # print(temparr)
         return temparr
 
 def uniformcostsearch(puzzleObj, puzzleNumber):
@@ -168,33 +184,40 @@ def uniformcostsearch(puzzleObj, puzzleNumber):
     open_list = []
     closed_list = []
 
+    #Initalize all the values based on the puzzle that was passed.
     currentArray = puzzleObj.array
     currentGasArray = puzzleObj.gas
     currentState = puzzleObj
     closed_list.append(puzzleObj)
 
-    # print(currentArray)
     movesLeft = False
+    #Loop while the game isn't done and while there are still moves left.
     while (not (puzzleObj.isgamedone(currentArray)) and movesLeft == False):
         currentPossMoves = puzzleObj.possmoves(currentArray, currentGasArray)
 
+        #Look at every possible move.
         for move in currentPossMoves:
             carBeingMoved = move[0]
             newCost = currentState.cost
+            #If the move is not the same as the last (not a car moving to the right one time again) increase the cost.
             if not (move == currentState.previousMove):
                 newCost = currentState.cost + 1
             with open(f".\search\\ucs-search-{puzzleNumber}.txt", "a") as sol:
                 sol.write(f"\n{newCost}\t{newCost}\t0\t{currentArray[0][0:6]}{ currentArray[1][0:6]}{ currentArray[2][0:6]}{ currentArray[3][0:6]}{currentArray[4][0:6]}{currentArray[5][0:6]}")
+            #Update arrays
             newGasArray = copy.deepcopy(currentGasArray)
             newEntry = {carBeingMoved: newGasArray.get(carBeingMoved) - 1}
             newGasArray.update(newEntry)
 
+            #Create a temporary array containing the array after the move is applied.
             tempArray = puzzleObj.movecar(currentArray, move)
 
+            #If we can valet  a car that is not the ambulance remove it.
             if puzzleObj.canValet(tempArray) and tempArray[2][5] != 'A':
                 tempArray = puzzleObj.removeValet(tempArray)
 
             # print(type(puzzle))
+            #Create a a new state based on the values set above
             newState = puzzle.__new__(puzzle)
             newState.__init__(tempArray, newGasArray)
             newState.cost = newCost
@@ -204,10 +227,12 @@ def uniformcostsearch(puzzleObj, puzzleNumber):
             newState.carsizes = puzzleObj.carsizes
 
             inClosedState = False
+            #Check if the state exists in the closed states.
             for closedState in closed_list:
                 if np.array_equal(closedState.array, newState.array):
                     inClosedState = True
 
+            #Check if the state exists in the open states and if it does, select the 'cheapest'
             inOpenState = False
             for openState in open_list:
                 if np.array_equal(openState.array, newState.array):
@@ -215,13 +240,18 @@ def uniformcostsearch(puzzleObj, puzzleNumber):
                         openState.cost = newState.cost
                     inOpenState = True
 
+            #If the state is not in the open or closed state, add it to the open list.
             if not (inOpenState) and not (inClosedState):
                 open_list.append(newState)
+
+        #See if the state is already in the closed list once again.
         alreadyInClosedList = False
         for value in range(len(closed_list)):
             if np.array_equal(currentState, closed_list[value]):
                 alreadyInClosedList = True
 
+        #If there are no more moves in the open list and there are no more possible moves, cars don't have any gas left
+        #or it's already in the closed list, print that there is no solution.
         if len(open_list) == 0 and (len(puzzleObj.possmoves(closed_list[len(closed_list) - 1].array, closed_list[
             len(closed_list) - 1].gas)) == 0 or alreadyInClosedList):
             print("No Solution")
@@ -231,15 +261,18 @@ def uniformcostsearch(puzzleObj, puzzleNumber):
 
         lowestCost = open_list[0].cost
 
+        #To select the next move, we look for the lowest cost in the open moves.
         for openPuzzle in open_list:
             if openPuzzle.cost < lowestCost:
                 lowestCost = openPuzzle.cost
 
         puzzleArray = []
+        #Create an array containing all the states whose cost is the lowest cost.
         for openPuzzle in open_list:
             if lowestCost == openPuzzle.cost:
                 puzzleArray.append(openPuzzle)
 
+        #Append the values from the first values in the puzzle array to the closed list and remove it from the open list.
         closed_list.append(puzzleArray[0])
         open_list.remove(puzzleArray[0])
         currentState = puzzleArray[0]
@@ -320,9 +353,11 @@ def GBFS(puzzleObj, heuristicNum, puzzleNumber):
 
     # print(currentArray)
     movesLeft = False
+    #Loop while the game isn't done and while there are still moves left.
     while (not (puzzleObj.isgamedone(currentArray)) and movesLeft == False):
         currentPossMoves = puzzleObj.possmoves(currentArray, currentGasArray)
 
+        #Iterate through all the possible moves of this state.
         for move in currentPossMoves:
             carBeingMoved = move[0]
             newCost = currentState.cost
@@ -333,6 +368,8 @@ def GBFS(puzzleObj, heuristicNum, puzzleNumber):
 
             tempArray = puzzleObj.movecar(currentArray, move)
 
+            #If this is not a move of the same car in the same direction, set the cost based on the heuristic value
+            #passed in the function.
             if not (move == currentState.previousMove):
                 if heuristicNum == 1:
                     newCost = puzzleObj.h1(tempArray)
@@ -355,6 +392,7 @@ def GBFS(puzzleObj, heuristicNum, puzzleNumber):
                         sol.write(
                             f"\n{(newCost)}\t0\t{newCost}\t{currentArray[0][0:6]}{currentArray[1][0:6]}{currentArray[2][0:6]}{currentArray[3][0:6]}{currentArray[4][0:6]}{currentArray[5][0:6]}")
 
+            #If there is a car in valet that is not the ambulance, remove it.
             if puzzleObj.canValet(tempArray) and tempArray[2][5] != 'A':
                 tempArray = puzzleObj.removeValet(tempArray)
 
@@ -367,11 +405,13 @@ def GBFS(puzzleObj, heuristicNum, puzzleNumber):
             newState.horver = puzzleObj.horver
             newState.carsizes = puzzleObj.carsizes
 
+            #Check whether the state is in the closed state list already.
             inClosedState = False
             for closedState in closed_list:
                 if np.array_equal(closedState.array, newState.array):
                     inClosedState = True
 
+            #Check whether the state is in the open list already and if it is, check which has the lowest cost.
             inOpenState = False
             for openState in open_list:
                 if np.array_equal(openState.array, newState.array):
@@ -379,13 +419,18 @@ def GBFS(puzzleObj, heuristicNum, puzzleNumber):
                         openState.cost = newState.cost
                     inOpenState = True
 
+            #If it is not in the open or closed state append it to the open list.
             if not (inOpenState) and not (inClosedState):
                 open_list.append(newState)
+
+        #Check once again if the value is already in the closed list.
         alreadyInClosedList = False
         for value in range(len(closed_list)):
             if np.array_equal(currentState, closed_list[value]):
                 alreadyInClosedList =True
 
+        #If there are no more moves in the open list and there are no more possible moves, cars don't have any gas left
+        #or it's already in the closed list, print that there is no solution.
         if len(open_list)==0 and (len(puzzleObj.possmoves(closed_list[len(closed_list)-1].array, closed_list[len(closed_list)-1].gas))==0 or alreadyInClosedList):
             print("No Solution")
             with open(f".\solutions\GBFS-{heuristicNum}-sol-{puzzleNumber}.txt","w+") as sol:
@@ -393,15 +438,18 @@ def GBFS(puzzleObj, heuristicNum, puzzleNumber):
             break
         lowestCost = open_list[0].cost
 
+        #Iterate through the open list to find the lowest cost.
         for openPuzzle in open_list:
             if openPuzzle.cost < lowestCost:
                 lowestCost = openPuzzle.cost
 
+        #Append all values with the lowest cost to an array.
         puzzleArray = []
         for openPuzzle in open_list:
             if lowestCost == openPuzzle.cost:
                 puzzleArray.append(openPuzzle)
 
+        #Append the first value with the lowest cost to the closed list and remove it from the open list.
         closed_list.append(puzzleArray[0])
         open_list.remove(puzzleArray[0])
         currentState = puzzleArray[0]
@@ -479,15 +527,18 @@ def AStar(puzzleObj, heuristicNum, puzzleNumber):
 
     # print(currentArray)
     movesLeft = False
+    #Loop while the game isn't done and while there are still moves left.
     while (not (puzzleObj.isgamedone(currentArray)) and movesLeft == False):
         currentPossMoves = puzzleObj.possmoves(currentArray, currentGasArray)
 
+        #Check for all the possible moves.
         for move in currentPossMoves:
             carBeingMoved = move[0]
             newDistanceCost = currentState.distanceTravelled
+
+            #If the move is the same as the last, set it's cost to the g(n) of the previous move.
             if not (move == currentState.previousMove):
                 newDistanceCost = currentState.distanceTravelled
-
 
             newGasArray = copy.deepcopy(currentGasArray)
             newEntry = {carBeingMoved: newGasArray.get(carBeingMoved) - 1}
@@ -496,6 +547,7 @@ def AStar(puzzleObj, heuristicNum, puzzleNumber):
             tempArray = puzzleObj.movecar(currentArray, move)
             newCost = currentState.cost
 
+            #Alter the cost of the state based on the heuristic value that was passed in.
             if not (move == currentState.previousMove):
                 if heuristicNum == 1:
                     newCost = puzzleObj.h1(tempArray) + newDistanceCost
@@ -517,6 +569,8 @@ def AStar(puzzleObj, heuristicNum, puzzleNumber):
                     with open(f".\search\AStar-{heuristicNum}-search-{puzzleNumber}.txt", "a") as sol:
                         sol.write(
                             f"\n{(puzzleObj.h4(tempArray) + currentState.distanceTravelled)}\t{currentState.distanceTravelled}\t{puzzleObj.h4(tempArray)}\t{currentArray[0][0:6]}{currentArray[1][0:6]}{currentArray[2][0:6]}{currentArray[3][0:6]}{currentArray[4][0:6]}{currentArray[5][0:6]}")
+
+            #Check if we can valet a car that is not the ambulance.
             if puzzleObj.canValet(tempArray) and tempArray[2][5] != 'A':
                 tempArray = puzzleObj.removeValet(tempArray)
 
@@ -530,11 +584,13 @@ def AStar(puzzleObj, heuristicNum, puzzleNumber):
             newState.horver = puzzleObj.horver
             newState.carsizes = puzzleObj.carsizes
 
+            #Check if the state is in the closed state list.
             inClosedState = False
             for closedState in closed_list:
                 if np.array_equal(closedState.array, newState.array):
                     inClosedState = True
 
+            #Check if the state is in the open state list, if it is, check if it is cheaper than the one currently there.
             inOpenState = False
             for openState in open_list:
                 if np.array_equal(openState.array, newState.array):
@@ -542,31 +598,38 @@ def AStar(puzzleObj, heuristicNum, puzzleNumber):
                         openState.cost = newState.cost
                     inOpenState = True
 
+            #Append the state based on whether or not it was found in the open or closed state list.
             if not (inOpenState) and not (inClosedState):
                 open_list.append(newState)
 
+        #Check once again if the value is already in the closed list.
         alreadyInClosedList = False
         for value in range(len(closed_list)):
             if np.array_equal(currentState, closed_list[value]):
                 alreadyInClosedList = True
 
+        #If there are no more moves in the open list and there are no more possible moves, cars don't have any gas left
+        #or it's already in the closed list, print that there is no solution.
         if len(open_list) == 0 and (len(puzzleObj.possmoves(closed_list[len(closed_list) - 1].array, closed_list[
             len(closed_list) - 1].gas)) == 0 or alreadyInClosedList):
             print("No Solution")
             with open(f".\solutions\AStar-{heuristicNum}-sol-{puzzleNumber}.txt","w+") as sol:
                 sol.write("No Solution")
             break
-        lowestCost = open_list[0].cost
 
+        lowestCost = open_list[0].cost
+        #Find the lowest cost in the open list.
         for openPuzzle in open_list:
             if openPuzzle.cost < lowestCost:
                 lowestCost = openPuzzle.cost
 
         puzzleArray = []
+        #Put all of the values with the lowest cost into an array.
         for openPuzzle in open_list:
             if lowestCost == openPuzzle.cost:
                 puzzleArray.append(openPuzzle)
 
+        #Append the lowest cost array to the closed list and remove it from the open list.
         closed_list.append(puzzleArray[0])
         open_list.remove(puzzleArray[0])
         currentState = puzzleArray[0]
@@ -672,19 +735,20 @@ for line in lines:
             cargas[currcarletter] = currcargas
             gascount = gascount + 1
 
+    #Set the gas for cars that did not have a gas value set for them.
     for car in carsingame:
         stringtochararray = list(car)
         test = stringtochararray
         if car not in cargas.keys():
             cargas[car] = 100
 
-# print(cargas)
 
 # Go through all the cars in the game and create a dictionary which tracks which way it can move.
     for car in carsingame:
         currarr = np.argwhere(array == car)
         row1 = currarr[0][0]
         row2 = currarr[1][0]
+        #Set the axis of the car based on whether their row values are the same or not.
         if row1 == row2:
             horver[car] = "h"
         else:
